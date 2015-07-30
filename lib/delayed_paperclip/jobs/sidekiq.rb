5d77a13247cf60f5d0abc4e609a4052a03410d53
hewrin <hewrin@hotmail.com>
2015-07-30 08:42:21 +0800

    removed logging statements

--- lib/delayed_paperclip/jobs/sidekiq.rb
+++ lib/delayed_paperclip/jobs/sidekiq.rb
@@ -7,25 +7,12 @@ module DelayedPaperclip
       sidekiq_options :queue => :paperclip
 
       def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-        puts "*********************" 
-        puts "enqueue_delayed_paperclip method starts"
-        puts "*********************"
         perform_async(instance_klass, instance_id, attachment_name)
-        #Image.find(instance_id).update(finished: true)
-        puts "*********************" 
-        puts "prepare_enqueueing_for_ method ends"
-        puts "*********************"
       end
 
       def perform(instance_klass, instance_id, attachment_name)
-        puts "*********************" 
-        puts "perforn method starts"
-        puts "*********************"
         DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name)
         Image.find(instance_id).update_columns(finished: true)
-        puts "*********************" 
-        puts "perforn method ends"
-        puts "*********************"
       end
     end
   end
